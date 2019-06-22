9b57e9259e984e2a905aed0ff1cf5813e6444be9
Lucia CL <luciacoucelamas@gmail.com>
2019-06-22 14:10:49 +0100

    data and basic structure with Header, Main and Footer

---
+++ src/frontend/components/footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Footer.css'
+
+export default function Footer() {
+    return (
+        <footer className='footer'>
+         <div className='footer-text'><p>© 2019 Orkestro | All Rights Reserved</p></div>
+        </footer>
+    );
+}
